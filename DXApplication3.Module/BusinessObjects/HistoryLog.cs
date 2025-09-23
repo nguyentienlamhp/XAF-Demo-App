a4da3915c727bcd267e40801721be46c3a8b3af9
nguyentienlam <mrlam2605@gmail.com>
2025-09-23 09:31:15 +0700

    Thêm trường requried , mask edit cho loại datetime

--- DXApplication3.Module/BusinessObjects/HistoryLog.cs
+++ DXApplication3.Module/BusinessObjects/HistoryLog.cs
@@ -1,12 +1,17 @@
 ﻿using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Map.Native;
 using DevExpress.Persistent.Base;
 using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.Persistent.Validation;
+using Microsoft.CodeAnalysis.Diagnostics;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static DevExpress.DataProcessing.InMemoryDataProcessor.AddSurrogateOperationAlgorithm;
 
 namespace DXApplication3.Module.BusinessObjects
 {
@@ -19,22 +24,30 @@ namespace DXApplication3.Module.BusinessObjects
         //[Key]
         //public Guid Id { get; set; }
 
-        [Required]
-        [StringLength(100)]
+     
+        [StringLength(100, ErrorMessage = "Hành động không được vượt quá 100 ký tự")]
+        [Display(Name = "Hành động")]
         public virtual string Action { get; set; } // Ví dụ: "Tạo", "Sửa", "Xóa"
 
-        [Required]
+        [ModelDefault("DisplayFormat", "{0:dd/MM/yyyy HH:mm:ss}")]
+        [ModelDefault("EditMask", "dd/MM/yyyy HH:mm:ss")]
+        [XafDisplayName("Thời gian")]
+        [RuleRequiredField("MyEntity_Timestamp_Required", DefaultContexts.Save, "Thời gian là bắt buộc")]
         public virtual DateTime Timestamp { get; set; }
 
-        [StringLength(100)]
+        [StringLength(100, ErrorMessage = "Tên người dùng không được vượt quá 100 ký tự")]
+        [Display(Name = "Tên người dùng")]
         public virtual string UserName { get; set; } // Tên user thực hiện hành động
 
-        [StringLength(100)]
+        [StringLength(100, ErrorMessage = "Loại đối tượng không được vượt quá 100 ký tự")]
+        [Display(Name = "Loại đối tượng")]
         public virtual string ObjectType { get; set; } // Ví dụ: "Task", "Employee"
 
+        [Display(Name = "ID đối tượng")]
         public virtual Guid ObjectId { get; set; } // ID của đối tượng liên quan
 
-        [StringLength(500)]
+        [StringLength(500, ErrorMessage = "Mô tả không được vượt quá 500 ký tự")]
+        [Display(Name = "Mô tả")]
         public virtual string Description { get; set; } // Mô tả chi tiết hành động
     }
 }
